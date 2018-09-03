066e23643f5c938d1b16c38bb464b26ca1382b4b
CMURPH56 <CameronMurphy87@gmail.com>
2018-09-03 15:15:46 -0500

    fixed issues added flashlight control script

---
+++ EndGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+	public Text LoseText;
+	void start(){
+		LoseText.text ="";
+	}
+
+	
+	void OnTriggerEnter(Collider other){
+		Destroy(other.gameObject);
+		LoseText.text = "You Lose";
+	}
+}
---
+++ EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+	public int speed;
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyLook();
+		EnemyMove();
+	}
+
+	void EnemyLook(){
+		transform.LookAt(transform.position + player.transform.rotation * Vector3.left, player.transform.rotation * Vector3.up);
+	}
+
+
+	void EnemyMove(){
+		Vector3 localPosition = player.transform.position - transform.position;
+		localPosition  = localPosition.normalized;
+		transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed );
+	}
+}
