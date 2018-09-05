bc893869886f83ff482152474aba32eceed32a46
CMURPH56 <CameronMurphy87@gmail.com>
2018-09-04 20:05:07 -0500

    testing using git with visual studio code

--- FlashLightControl.cs
+++ FlashLightControl.cs
@@ -7,7 +7,7 @@ public class FlashLightControl : MonoBehaviour {
 
 	public bool on = false;
 	public Light light;
-	
+	//git test wit visual studio code
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.F))
 			on = !on;
